0dbee01ff9c74351ea8aa1e22debbc520c99b3ca
andyosuna <andyosuna@gmail.com>
2014-10-30 17:18:59 -0700

    added index

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en-us">
   <head>
+
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta charset="UTF-8">
+
     <title></title>
+
   </head>
   <body>
-    
+    <header>
+      
+    </header>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="sixcol"></div>
+          <div class="sixcol"></div>
+        </div>
+      </div>
+    </footer>
   </body>
 </html>
